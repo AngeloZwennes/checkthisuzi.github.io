ad606792574e4778fcb0e420091c5d0ea1085217
Angelo <Angelo_93@live.nl>
2017-04-03 15:45:31 +0200

    oepsie scheldwoord staat nog in de comments haha

--- scripts/filters/capitalize.js
+++ scripts/filters/capitalize.js
@@ -1,4 +1,3 @@
-//tering simpel beetje hoofdletters neergooien
 app.filter("capitalize",function(){
     return function(input) {
         return (!!input) ? input.charAt(0).toUpperCase() + input.substr(1).toLowerCase() : '';
