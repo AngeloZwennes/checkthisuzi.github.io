b6c70b6a2857a40ca9ada8cfb61bf9cefa9f46c5
Angelo <Angelo_93@live.nl>
2017-04-03 02:56:02 +0200

    fixed data when the store is empty.

--- scripts/factories/customersfactory.js
+++ scripts/factories/customersfactory.js
@@ -4,7 +4,7 @@ app.factory('CustomersFactory',function(GUIDFactory){
         //localStorage.removeItem("customers");
         var localCustomers = JSON.parse(localStorage.getItem("customers"));
         if(localCustomers == undefined || localCustomers.length < 0){
-            localCustomers = [];
+            localCustomers = [{"name":"Pim","email":"pim@email.nl","adress":"ergens","city":"zoetermeer","id":"78cc0023-1e35-45b1-293e-cab3de0418e1","$$hashKey":"object:51"},{"name":"Angelo","email":"angelo@email.nl","adress":"lommerbaan","city":"zoetermeerrr","$$hashKey":"object:9"},{"name":"bodhie","email":"bodie@email.nl","adress":"ergens","city":"goudaa","$$hashKey":"object:10"},{"name":"Jacky","email":"Jacky@email.nl","adress":"Ergens","city":"Zoetermeer","id":"eafbbd90-2f33-49e0-4a25-39f5b12d6cd4"},{"name":"Shawn","email":"Shawn@autist.nl","adress":"denhaagzonderhoofdletter","city":"denhaag","id":"57e4729e-938a-42e8-4f9e-de59c8758c53"}];
             localStorage.setItem("customers",JSON.stringify(localCustomers));
         }
         return JSON.parse(localStorage.getItem("customers"));
